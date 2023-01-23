44c92d2380bc67cf8b55a40ac6a38f5b21f80246
Hizaram <promisevector@gmail.com>
2023-01-23 19:32:45 +0100

    Added the function that returns the index_range given the page and page size

---
+++ 0x00-pagination/0-simple_header_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that contains a function that handles pagination parameters
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size
+    Args: page (int): page specified
+          page_size (int): page size specified
+    Returns: Tuple of the index_range
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start index, end index)
