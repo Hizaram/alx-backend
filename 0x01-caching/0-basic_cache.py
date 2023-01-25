fd41509e2b65b50581c422dee4f898dbd23f0342
Hizaram <promisevector@gmail.com>
2023-01-25 21:05:17 +0100

    Added the class BasicCache that allows the addition and retrieval of items in a cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module that contains the class BasicCache"""
+from parent_class import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Class BasicCache that represents an object that allows storing
+    and retrieval of items from a dictionary
+    """
+    def put(self, key,item):
+        """Method that adds an object to the dictionary using the key"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Method that retrieves an item from cache using the key"""
+        return self.cache_data.get(key, None)
